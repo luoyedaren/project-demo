8f1629522a97287b5213263d1b218e502a69f84e
chenghai <287623909@qq.com>
2018-12-05 16:37:49 +0800

    demo

--- spring-abc/springboot-one/src/main/java/com/ck/labs/abc/springboot/one/controller/Example1Controller.java
+++ spring-abc/springboot-one/src/main/java/com/ck/labs/abc/springboot/one/controller/Example1Controller.java
@@ -1,5 +1,6 @@
 package com.ck.labs.abc.springboot.one.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class Example1Controller {
+
+	@Autowired
 
 	@GetMapping("example/1")
 	public String one(){
