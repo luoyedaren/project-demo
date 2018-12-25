57afd7ff09a0276e9105cd58fff1795fb80a73cc
chenghai <287623909@qq.com>
2018-12-25 09:38:44 +0800

    java温故

---
+++ alljava/src/main/java/lang/interfaces/SingWriter.java
@@ -0,0 +1,11 @@
+package lang.interfaces;
+
+/**
+ * project alljava
+ *
+ * @author chenghai on 2018/12/24 0024. - 星期一
+ * nickName louyedaren
+ */
+public interface SingWriter extends Singer,Writer {
+
+}
