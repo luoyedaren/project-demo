97e485df2f302e8052f12094b41a497752641e13
chenghai <287623909@qq.com>
2018-12-25 09:38:08 +0800

    java温故

---
+++ alljava/src/main/java/lang/interfaces/SingWriterImpl.java
@@ -0,0 +1,21 @@
+package lang.interfaces;
+
+import lang.interfaces.SingWriter;
+
+/**
+ * project alljava
+ *
+ * @author chenghai on 2018/12/24 0024. - 星期一
+ * nickName louyedaren
+ */
+public class SingWriterImpl implements SingWriter {
+	@Override
+	public void sing() {
+
+	}
+
+	@Override
+	public void write() {
+
+	}
+}
